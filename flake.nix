6bf870a0d87a1142d616ef27713eec6c96e72e1b
Jacob Miller <Jamiller219@gmail.com>
2026-05-26 21:49:58 -0500

    deps: add nominatim build deps

--- flake.nix
+++ flake.nix
@@ -34,6 +34,43 @@
           default = pkgs.mkShellNoCC {
             packages = with pkgs; [
               self.formatter.${system}
+              postgresql
+              postgresqlPackages.postgis
+              osm2pgsql
+              python313
+              # this is nominatim-db
+              nominatim
+              python313Packages.nominatim-api
+              # deps
+              python313Packages.psycopg
+              python313Packages.python-dotenv
+              python313Packages.psutil
+              python313Packages.jinja2
+              python313Packages.pyicu
+              python313Packages.pyyaml
+              python313Packages.mwparserfromhell
+              python313Packages.pyosmium
+              python313Packages.sqlalchemy
+              # asyncpg is apparently not necessary since we are using sqlalchemy >= 2.0
+              # but we will include it anyways
+              python313Packages.asyncpg
+              python313Packages.falcon
+              python313Packages.starlette
+              python313Packages.uvicorn
+
+              # Developer deps:
+
+              python313Packages.flake8
+              python313Packages.mypy
+              python313Packages.pytest
+              python313Packages.pytest-asyncio
+              python313Packages.pytest-bdd
+              python313Packages.httpx
+              python313Packages.asgi-lifespan
+              python313Packages.mkdocs
+              python313Packages.mkdocstrings
+              python313Packages.mkdocs-material
+              python313Packages.mkdocs-gen-files
             ];
           };
         }
